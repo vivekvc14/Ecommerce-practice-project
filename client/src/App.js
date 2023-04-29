1c436fa46a11d19b5e582fd77b225d069c2ed415
vivek <chimnanivivek14@gmail.com>
2023-04-29 16:15:41 +0530

    Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import Root from "./pages/Root";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import Product from "./pages/Product";
+import Landing from "./pages/Landing";
 
 function App() {
 
   const router = createBrowserRouter([
     {
       path: "/", element: <Root />, children: [
+        { index: true, element: <Landing /> },
         { path: "products", element: <Products /> },
         { path: "product/:productId", element: <Product /> },
         { path: "cart", element: <Cart /> },
