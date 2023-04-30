4265a620b61cdf149dda0fe178c4a00c86c136c4
vivek <chimnanivivek14@gmail.com>
2023-04-30 16:56:11 +0530

    Token verification middleware

---
+++ server/tokens/verifyToken.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken")
+const User = require("../models/User")
+
+const verifyToken = async (req, res, next) => {
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        const token = req.headers.authorization.split(" ")[1]
+        if (!token) {
+            return res.status(500).json("Not Authorized")
+        } else {
+            const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
+            if (!decoded) {
+                return res.status(500).json("Not Authorized")
+            }
+
+            req.user = await User.findById(decoded.id)
+            next()
+        }
+    } else {
+        return res.status(500).json("Not Authorized")
+    }
+}
+
+module.exports = verifyToken
