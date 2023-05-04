c2d8cbe9f61652c377db363abaa24bd5a5904605
vivek <chimnanivivek14@gmail.com>
2023-05-04 11:57:05 +0530

    Create review endpoint in backend

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,4 +1,5 @@
 const Product = require("../models/Product")
+const User = require("../models/User")
 
 exports.getProducts = async (req, res) => {
     const products = await Product.find()
@@ -11,5 +12,42 @@ exports.getProduct = async (req, res) => {
         res.json(product)
     } else {
         return res.status(404).json("Product not found")
+    }
+}
+
+exports.createReview = async (req, res) => {
+    const { userId, rating, title, comment } = req.body;
+    try {
+        const product = await Product.findById(req.params.productId)
+        const user = await User.findById(userId)
+        if (req.user._id.toString() === user._id.toString()) {
+            if (product) {
+                const alreadyReviewed = product.reviews.find(review => review.user.toString() === user._id.toString())
+                if (alreadyReviewed) {
+                    return res.status(400).json("Product already reviewed")
+                }
+
+                const review = {
+                    name: user.name,
+                    rating,
+                    comment,
+                    title,
+                    user: user._id
+                }
+
+                product.reviews.push(review)
+                product.numberOfReviews = product.reviews.length;
+                product.rating = product.reviews.reduce((total, review) => total += review.rating, 0) / product.reviews.length;
+
+                await product.save()
+                res.status(201).json({ message: "Review has been saved!" })
+            } else {
+                return res.status(404).json("Product not found!")
+            }
+        } else {
+            return res.status(402).json("You are not authorized.")
+        }
+    } catch (error) {
+        res.status(500).json("Something went wrong, please try again.")
     }
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -2,9 +2,12 @@ const express = require("express")
 const routes = express.Router()
 
 const productControllers = require('../controllers/product')
+const verifyToken = require("../tokens/verifyToken")
 
 routes.get("/", productControllers.getProducts)
 
 routes.get("/:productId", productControllers.getProduct)
+
+routes.post("/review/:productId", verifyToken, productControllers.createReview)
 
 module.exports = routes;
