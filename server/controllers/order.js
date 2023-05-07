80cfecb5a8a8b1817aa0e5c9f8e1cb129d30c6ac
vivek <chimnanivivek14@gmail.com>
2023-05-07 11:47:00 +0530

    Order routes and controllers in backend

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -19,13 +19,45 @@ exports.createOrder = async (req, res) => {
     }
 }
 
-exports.getOrders = async (req, res) => {
+exports.getUserOrders = async (req, res) => {
     try {
         const orders = await Order.find({ user: req.params.userId }).populate(["user", "orderItems.product"])
         if (!orders) {
             return res.status(402).json("Orders not found")
         }
         return res.json(orders)
+    } catch (err) {
+        return res.status(500).json("Something went wrong, please try again!")
+    }
+}
+
+exports.getOrders = async (req, res) => {
+    try {
+        const orders = await Order.find().populate(["user", "orderItems.product"])
+        if (!orders) {
+            return res.status(402).json("Orders not found")
+        }
+        return res.json(orders)
+    } catch (err) {
+        return res.status(500).json("Something went wrong, please try again!")
+    }
+}
+
+exports.deleteOrder = async (req, res) => {
+    try {
+        await Order.findByIdAndDelete(req.params.orderId)
+        return res.json("Order deleted succesffully")
+    } catch (err) {
+        return res.status(500).json("Something went wrong, please try again!")
+    }
+}
+
+exports.updateOrder = async (req, res) => {
+    try {
+        const order = await Order.findById(req.params.orderId)
+        order.isDelivered = true;
+        const updatedOrder = await order.save()
+        res.json(updatedOrder)
     } catch (err) {
         return res.status(500).json("Something went wrong, please try again!")
     }
--- server/routes/order.js
+++ server/routes/order.js
@@ -3,9 +3,16 @@ const routes = express.Router()
 
 const orderControllers = require("../controllers/order");
 const verifyToken = require("../middleware/tokens/verifyToken");
+const verifyAdmin = require("../middleware/verifyAdmin");
 
 routes.post("/", verifyToken, orderControllers.createOrder)
 
-routes.get("/:userId", verifyToken, orderControllers.getOrders)
+routes.get("/:userId", verifyToken, orderControllers.getUserOrders)
+
+routes.get("/", verifyToken, verifyAdmin, orderControllers.getOrders)
+
+routes.delete("/:orderId", verifyToken, verifyAdmin, orderControllers.deleteOrder)
+
+routes.put("/:orderId", verifyToken, verifyAdmin, orderControllers.updateOrder)
 
 module.exports = routes;
