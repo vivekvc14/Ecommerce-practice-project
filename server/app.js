9b142200e6937c6652acf146147c1825d48969e5
vivek <chimnanivivek14@gmail.com>
2023-12-16 11:39:32 +0530

    Specified cors policy in server

--- server/app.js
+++ server/app.js
@@ -1,21 +1,23 @@
-const express = require("express")
-const connectToDatabase = require("./database")
-require("dotenv").config()
+const express = require("express");
+const connectToDatabase = require("./database");
+require("dotenv").config();
+const cors = require("cors");
 
-const app = express()
-connectToDatabase()
-app.use(express.json())
+const app = express();
+connectToDatabase();
+app.use(cors());
+app.use(express.json());
 
-const productRoutes = require("./routes/product")
-const userRoutes = require("./routes/user")
-const orderRoutes = require("./routes/order")
+const productRoutes = require("./routes/product");
+const userRoutes = require("./routes/user");
+const orderRoutes = require("./routes/order");
 
-app.use("/product", productRoutes)
-app.use("/user", userRoutes)
-app.use("/order", orderRoutes)
+app.use("/product", productRoutes);
+app.use("/user", userRoutes);
+app.use("/order", orderRoutes);
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
